517f349102b41a37e881797b0651bbddc7f00c86
Stef <s.kraaijeveld3@student.avans.nl>
2024-04-19 12:50:51 +0200

    Test project

---
+++ OpenCV/Test.cpp
@@ -0,0 +1,22 @@
+#ifdef _DEBUG
+#pragma comment ( lib, "opencv_world470d.lib" )
+#else
+#pragma comment ( lib, "opencv_world470.lib" )
+#endif
+
+
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main() {
+	string path = "cat.png";
+	Mat img = imread(path);
+	imshow("Image", img);
+	waitKey(0);
+	return 0;
+}
